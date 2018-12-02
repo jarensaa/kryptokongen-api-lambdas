095f1dfef34a672eaef8c5ae00ae9b01a6383635
jarensaa <jens.rensaa@gmail.com>
2018-12-02 09:01:42 +0100

    replace birthday gratulation with normal behaviour

--- src/main/java/kryptokongen/kryptokongen.java
+++ src/main/java/kryptokongen/kryptokongen.java
@@ -3,19 +3,14 @@ package kryptokongen;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class kryptokongen implements RequestHandler<Object,String> {
-    public String handleRequest(Object o, Context context) {
-        double randomNumber = Math.random();
-        if(randomNumber > 0.5){
-            return "Been";
-        }
+public class kryptokongen implements RequestHandler<Object, String> {
+  public String handleRequest(Object o, Context context) {
+    double randomNumber = Math.random();
 
-        if (randomNumber < 0.05) {
-            return "Svenskene";
-        }
-
-        return "Jens";
+    if (randomNumber > 0.8) {
+      return "Jens";
     }
+
+    return "Been";
+  }
 }
-
-
